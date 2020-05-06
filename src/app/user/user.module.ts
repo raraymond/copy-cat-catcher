e5db1d2b198af8bf9001c9fd0d68e9c79333721d
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-06 18:51:03 -0400

    added email login

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { EmailLoginComponent } from './email-login/email-login.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective],
+  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule, MaterialModule,ReactiveFormsModule
   ]
 })
 export class UserModule { }
