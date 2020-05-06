97babb5b8f098b0b0977f55e71262679db6ca9b0
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-06 18:04:38 -0400

    added firebase imports and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+
+const config = {
+  apiKey: "AIzaSyAQXs38owsseq9wzk5tw4bSYLVCTTSUjqA",
+  authDomain: "copy-cat-catcher-348e7.firebaseapp.com",
+  databaseURL: "https://copy-cat-catcher-348e7.firebaseio.com",
+  projectId: "copy-cat-catcher-348e7",
+  storageBucket: "copy-cat-catcher-348e7.appspot.com",
+  messagingSenderId: "58313302603",
+  appId: "1:58313302603:web:9b21954d7d052b154aa3ce",
+  measurementId: "G-PCE4B3RNE4"
+};
+
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
   imports: [
@@ -15,6 +33,10 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent],
