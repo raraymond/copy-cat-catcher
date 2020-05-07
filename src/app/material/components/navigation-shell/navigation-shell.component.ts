6fd7019d1a9356283eccdf935b2dead032354932
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-07 08:23:11 -0400

    added conditional rendering for login/logout and gaurded routes links

--- src/app/material/components/navigation-shell/navigation-shell.component.ts
+++ src/app/material/components/navigation-shell/navigation-shell.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from 'src/app/user/services/auth.service';
 
 @Component({
   selector: 'app-navigation-shell',
@@ -16,6 +17,6 @@ export class NavigationShellComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, public auth: AuthService) {}
 
 }
