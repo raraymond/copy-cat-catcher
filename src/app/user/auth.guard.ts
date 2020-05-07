27734f3c54f2febdba6af5049b90db322f795369
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-06 23:29:43 -0400

    fixed authgaurd to work with new service and finished styling kanban

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,27 +1,48 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+// import { AngularFireAuth } from '@angular/fire/auth';
 import { SnackService } from '../services/snack.service';
+import {AuthService} from '../user/services/auth.service';
+import { Observable } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
-    private afAuth: AngularFireAuth,
-    private snack: SnackService
+    private auth: AuthService,
+    // private afAuth: AngularFireAuth,
+    private snack: SnackService,
+    private router: Router
   ) {}
 
-  async canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Promise<boolean> {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
-    const user = await this.afAuth.currentUser;
-    const isLoggedIn = !!user;
-    if (!isLoggedIn) {
-      this.snack.authError();
-    }
-    return isLoggedIn;
+      return this.auth.user$.pipe(
+        take(1),
+        map(user => !!user), // <-- map to boolean
+        tap(loggedIn => {
+          if (!loggedIn) {
+            console.log('access denied')
+            this.router.navigate(['/login']);
+          }
+      })
+    )
   }
 }
+
+  // async canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot
+  // ): Promise<boolean> {
+
+  //   const user = await this.afAuth.currentUser;
+  //   const isLoggedIn = !!user;
+  //   if (!isLoggedIn) {
+  //     this.snack.authError();
+  //   }
+  //   return isLoggedIn;
+  // }
+
